914711b482fa4c69190a793fc495b591e40f593a
jvpelai <jv.pelai@gmail.com>
2020-05-25 21:29:59 -0300

    functions updated, now It's getting different values.

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const startTimer = document.querySelector('.startTimer');
 const timerDisplay = document.querySelector('#timer');
+const pauseTimer = document.querySelector('.pause');
+let timeOptions = document.querySelectorAll("#sessionTime");
+
+const timer =  {
+    minutes: 0,
+    seconds: 0,
+    pauseSelected: false,
+}
+
 
 //gets the session time selected value
 function seshTime(){
     let sessionTime = document.getElementById("sessionTime");
     let time = sessionTime.options[sessionTime.selectedIndex].value;
     timeOptions = document.querySelectorAll("#sessionTime");
-    return time
+    timerDisplay.textContent = time+":00";
+    return time;
+}
+
+
+for(timeOption of timeOptions){
+    timeOption.addEventListener('click',() => {
+        let time = seshTime();
+        timer.minutes = time;
+    });
 }
 
 //gets the break time selected value
@@ -17,35 +35,46 @@ function brkTime(){
     return brk
 
 }
-const timer =  {
-    seconds: 0,
-    minutes: 10,
-}
 
-function handleTime (sec, min) {
-    console.log(timer);
-    if (sec == 0) {
-        sec = 59;
-        min--;
+startTimer.addEventListener('click', () => count());
+pauseTimer.addEventListener('click', (e) => handlePause(e));
+
+function handlePause(e) {
+    let {pauseSelected} = timer;
+    if (!pauseSelected) {
+        timer.pauseSelected = true;
+        e.target.textContent = 'play';
     } else {
-        sec--;
+        timer.pauseSelected = false;
+        e.target.textContent = 'pause';
     }
 }
 
-function displayTime(sec, min) {
-    handleTime(sec, min);
+function handleTime() {
+    let {minutes, seconds} = timer;
+    if (seconds == 0) {
+        timer.seconds = 59;
+        timer.minutes--;
+    } else {
+        timer.seconds--;
+    }
+}
+
+function displayTime() {
     console.log(timer);
-    secString = (sec < 10) ? `0${sec.toString()}` : sec.toString();
-    minString = min.toString();
+    handleTime();
     console.log(timer);
-    console.log(minString + secString);
+    let {minutes, seconds} = timer;
+    let minString = minutes.toString();
+    let secString = (seconds < 10) ? `0${seconds.toString()}` : seconds.toString();
+    console.log(minString, secString);
     timerDisplay.textContent = `${minString}:${secString}`;
 }
 
-function count(time) {
-    if (timer.minutes == 0 && timer.seconds == 0) {
-        return;
-    } else {
-        setInterval(displayTime(timer.seconds, timer.minutes), 1000);
-    }
+function count() {
+    setInterval(function () {
+        if ((timer.minutes == 0 && timer.seconds == 0) ||
+        timer.pauseSelected == true) return;   
+        displayTime();
+    }, 1000);
 }
